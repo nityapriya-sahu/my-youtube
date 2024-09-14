a1a2d3fa32c88aaf9f1c25e0335b17f23d1ce8c7
nityapriya-sahu <sahunityapriya@gmail.com>
2024-09-14 23:03:31 +0530

    search api added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CiSearch } from "react-icons/ci";
 import { IoMdNotificationsOutline } from "react-icons/io";
@@ -7,11 +7,33 @@ import { FaMicrophone } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import Yt_Logo from "../yt_logo-rbg.png";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 const Header = () => {
   const dispatch = useDispatch();
 
+  const [searchQuery, setSearchQuery] = useState("");
+  // console.log(searchQuery, "searchsearch");
+  const [suggestion, setSuggestion] = useState([]);
+
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
+  };
+  //Make an API call when typing in search bar
+  useEffect(() => {
+    // but if the diff betn 2 API calls < 200ms, then decline API call
+    const timer = setTimeout(() => {
+      getSearchSuggestion();
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestion = async () => {
+    const data = await fetch(`${YOUTUBE_SEARCH_API}${searchQuery}`);
+    const json = await data.json();
+    console.log(json[1], "JSON JSON");
+    setSuggestion(json[1]);
   };
   return (
     <div className="grid grid-flow-col shadow-md py-3 px-4 bg-white sticky top-0 w-full">
@@ -31,6 +53,8 @@ const Header = () => {
         <input
           type="text"
           name="search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
           className="border w-1/3 py-2 rounded-l-full border-violet px-4 border-r-0"
         />
         <button className="border py-3 px-6 rounded-r-full bg-gray border-violet">
